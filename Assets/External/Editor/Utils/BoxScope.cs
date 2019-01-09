101c5c4e35e8aeae69ef8a101375363e7deb39f4
lucas henriques <oktagon.engineer1@gmail.com>
2019-01-09 10:38:26 -0200

    missing files

---
+++ Assets/External/Editor/Utils/BoxScope.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BoxScope : System.IDisposable
+{
+    readonly bool indent;
+    static GUIStyle boxScopeStyle;
+    public static GUIStyle BoxScopeStyle
+    {
+        get
+        {
+            if (boxScopeStyle == null)
+            {
+                boxScopeStyle = new GUIStyle(EditorStyles.helpBox);
+                RectOffset p = boxScopeStyle.padding;
+                p.right += 6;
+                p.top += 1;
+                p.left += 3;
+                p.bottom += 2;
+            }
+
+            return boxScopeStyle;
+        }
+    }
+
+    public BoxScope(bool indent = false)
+    {
+        this.indent = indent;
+        EditorGUILayout.BeginVertical(BoxScopeStyle);
+
+        if (indent)
+            EditorGUI.indentLevel++;
+    }
+
+    public void Dispose()
+    {
+        if (indent)
+            EditorGUI.indentLevel--;
+        
+        EditorGUILayout.EndVertical();
+    }
+}
---
+++ Assets/External/OktNetworkMonitor/Sources/IMonitor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Oktagon.Network
+{
+    public interface IMonitor
+    {
+        void SetupMonitor(OktNetworkMonitor pMonitor);
+        void Destroy();
+
+        bool Record
+        {
+            get;
+            set;
+        }
+
+    }
+
+}
